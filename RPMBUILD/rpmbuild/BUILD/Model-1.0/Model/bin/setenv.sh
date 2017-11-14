2ca10e3d838e552e3dc7e2d08afbe887300769f2
eli <liu1101241@yahoo.com>
2017-11-14 17:08:37 +0800

    merge

--- RPMBUILD/rpmbuild/BUILD/Model-1.0/Model/bin/setenv.sh
+++
@@ -1,5 +0,0 @@
-#add tomcat pid
-ProjectName=`echo $CATALINA_BASE|awk -F '/' '{print $NF}'`
-CATALINA_PID=$CATALINA_BASE/${ProjectName}.pid
-#add java opts
-JAVA_OPTS="-Xms4096M -Xmx4096M -XX:PermSize=1024M -XX:MaxPermSize=1024M -Djava.awt.headless=true"
--- RPMBUILD/rpmbuild/BUILD/app01-1.0/app01/bin/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-basepath=$(cd `dirname $0`; pwd)
-User=zeasn
-sudo su - $User -c $basepath/startup.sh 
