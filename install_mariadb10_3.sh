79273ae39ef5683e002a22ea289b64c6904f746e
eli <liu11011241@yahoo.com>
2018-09-21 18:04:14 +0800

    for Ubuntu 16.04.5 LTS

---
+++ install_mariadb10_3.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+debconf-set-selections <<< "mariadb-server-10.3 root_password password 123456"
+debconf-set-selections <<< "mariadb-server-10.3 root_password_again password 123456"
+debconf-set-selections <<< "mariadb-server-10.3 mysql-server/root_password password 123456"
+debconf-set-selections <<< "mariadb-server-10.3 mysql-server/root_password_again password 123456"
+
+sudo apt-get install software-properties-common -y
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+sudo add-apt-repository 'deb [arch=amd64,arm64,i386,ppc64el] http://mariadb.mirror.anstey.ca/repo/10.3/ubuntu xenial main'
+
+sudo apt update
+sudo apt install mariadb-server -y
+
+test -e /etc/mysql/my.cnf && mv /etc/mysql/my.cnf /etc/mysql/my.cnf_$(date "+%Y%m%d")
+
+mkdir -p /data/{log/mariadb,mariadb} ; chown mysql.mysql /data/{mariadb,log/mariadb}
+
+echo "[client]
+port		= 3306
+socket		= /var/run/mysqld/mysqld.sock
+[mysqld_safe]
+socket		= /var/run/mysqld/mysqld.sock
+nice		= 0
+[mysqld]
+init_connect='SET collation_connection = utf8mb4_unicode_ci'
+init_connect='SET NAMES utf8mb4'
+character-set-server=utf8mb4
+collation-server=utf8mb4_unicode_ci
+skip-character-set-client-handshake
+skip-host-cache
+skip-name-resolve
+log_slave_updates  = 1
+log-error=/data/log/mariadb/error.log
+user		= mysql
+pid-file	= /var/run/mysqld/mysqld.pid
+socket		= /var/run/mysqld/mysqld.sock
+port		= 3306
+basedir		= /usr
+datadir		= /data/mariadb
+tmpdir		= /tmp
+lc_messages_dir	= /usr/share/mysql
+lc_messages	= en_US
+skip-external-locking
+bind-address		= 0.0.0.0
+max_connections		= 1000
+connect_timeout		= 3600
+wait_timeout		= 86400
+max_allowed_packet	= 16M
+thread_cache_size       = 128
+sort_buffer_size	= 4M
+bulk_insert_buffer_size	= 16M
+tmp_table_size		= 32M
+max_heap_table_size	= 32M
+myisam_recover_options = BACKUP
+key_buffer_size		= 128M
+table_open_cache	= 400
+myisam_sort_buffer_size	= 512M
+concurrent_insert	= 2
+read_buffer_size	= 2M
+read_rnd_buffer_size	= 1M
+query_cache_limit		= 128K
+query_cache_size		= 64M
+log_warnings		= 2
+slow_query_log_file	= /data/log/mariadb/mariadb-slow.log
+long_query_time = 10
+log_slow_verbosity	= query_plan
+server-id		= 2
+log_bin			= /data/mariadb/mariadb-bin
+log_bin_index		= /data/mariadb/mariadb-bin.index
+expire_logs_days	= 21
+max_binlog_size         = 100M
+default_storage_engine	= InnoDB
+innodb_buffer_pool_size	= 20G
+innodb_log_buffer_size	= 8M
+innodb_file_per_table	= 1
+innodb_open_files	= 400
+innodb_io_capacity	= 400
+innodb_flush_method	= O_DIRECT
+[galera]
+[mysqldump]
+quick
+quote-names
+max_allowed_packet	= 16M
+[mysql]
+[isamchk]
+key_buffer		= 16M
+!includedir /etc/mysql/conf.d/
+" > /etc/mysql/my.cnf
+
+mysql_install_db --datadir=/data/mariadb/ --user=mysql
+systemctl start mariadb
+systemctl enable mariadb
---
+++ install_rabbitmq37.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+
+tmp_dir=/tmp/$$
+source_erlang="/etc/apt/sources.list.d/erlang.list"
+source_rabbitmq="/etc/apt/sources.list.d/rabbitmq-server.list"
+plugins="https://dl.bintray.com/rabbitmq/community-plugins/3.7.x/rabbitmq_delayed_message_exchange/rabbitmq_delayed_message_exchange-20171201-3.7.x.zip"
+
+install_erlang21() {
+
+wget -O - 'https://dl.bintray.com/rabbitmq/Keys/rabbitmq-release-signing-key.asc' | sudo apt-key add -
+sudo echo "deb http://dl.bintray.com/rabbitmq/debian xenial erlang-21.x" > ${source_erlang}
+sudo apt update && apt install erlang-nox -y
+
+}
+
+install_rabbitmq() {
+
+wget -O - 'https://dl.bintray.com/rabbitmq/Keys/rabbitmq-release-signing-key.asc' | sudo apt-key add -
+sudo test -e ${source_erlang} && rm -f ${source_erlang}
+sudo echo "deb https://dl.bintray.com/rabbitmq/debian xenial main" > ${source_rabbitmq}
+sudo apt update && apt install rabbitmq-server -y
+
+#install plugins
+sudo apt install unzip -y
+test -d ${tmp_dir} || mkdir -p ${tmp_dir}
+cd ${tmp_dir} && wget ${plugins} 
+unzip `basename ${plugins}`
+sudo mv rabbitmq_delayed_message_exchange-20171201-3.7.x.ez /usr/lib/rabbitmq/lib/rabbitmq_server-3.7.7/plugins/
+
+#enable plugins
+sudo rabbitmq-plugins enable rabbitmq_management
+sudo rabbitmq-plugins enable rabbitmq_delayed_message_exchange
+
+#set admin passwd
+sudo rabbitmqctl add_user admin advance.ai2016
+sudo rabbitmqctl set_user_tags admin administrator
+sudo rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"
+
+#set started
+sudo systemctl enable rabbitmq-server
+
+test -d ${tmp_dir} && rm -rf ${tmp_dir}
+
+}
+
+install_erlang21 && install_rabbitmq
+
