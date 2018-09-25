2f3ff5d76c0c02f75a2ca3c25117e8b12606ff95
eli <liu11011241@yahoo.com>
2018-09-25 13:18:38 +0800

    fix

--- install_redis.sh
+++ install_redis.sh
@@ -8,7 +8,8 @@ cd ${tmp_dir} && wget -c http://download.redis.io/releases/redis-4.0.8.tar.gz
 tar -xvf redis-4.0.8.tar.gz;cd redis-4.0.8
 make && make install
 cd utils;curl -s https://raw.githubusercontent.com/narakus/script/master/install_server.sh | bash
-systemctl start redis_6379
+sed -i 's/bind 127.0.0.1/bind 0.0.0.0/' /etc/redis/redis.conf
+systemctl restart redis_6379
 systemctl enable redis_6379
 
 rm -rf  ${tmp_dir}
