f8f4ec558db8bb644dfd2c0338986c0cd8174052
eli <liu11011241@yahoo.com>
2018-09-25 13:12:24 +0800

    for ubuntu 16.04

---
+++ install_redis.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+tmp_dir=/tmp/$$
+
+mkdir -p ${tmp_dir}
+sudo apt-get install build-essential tcl wget -y
+cd ${tmp_dir} && wget -c http://download.redis.io/releases/redis-4.0.8.tar.gz
+tar -xvf redis-4.0.8.tar.gz;cd redis-4.0.8
+make && make install
+cd utils;curl -s https://raw.githubusercontent.com/narakus/script/master/install_server.sh | bash
+systemctl start redis_6379
+systemctl enable redis_6379
+
+rm -rf  ${tmp_dir}
