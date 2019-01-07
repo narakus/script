df4d6a89a963ed1078bfefbbf7a486d325fda1d9
eli <liu11011241@yahoo.com>
2019-01-07 17:44:31 +0800

    Create wget_jdk1.8.sh

---
+++ wget_jdk1.8.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget --continue --no-check-certificate -O jdk-8u191-linux-x64.tar.gz --header "Cookie: oraclelicense=a" http://download.oracle.com/otn-pub/java/jdk/8u191-b12/2787e4a523244c269598db4e85c51e0c/jdk-8u191-linux-x64.tar.gz
