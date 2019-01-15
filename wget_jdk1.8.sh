e40c040f9ace0d784356723e1a07cd57623881f3
eli <liu11011241@yahoo.com>
2019-01-15 11:04:31 +0800

    Update wget_jdk1.8.sh

--- wget_jdk1.8.sh
+++ wget_jdk1.8.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-wget --continue --no-check-certificate -O jdk-8u191-linux-x64.tar.gz --header "Cookie: oraclelicense=a" http://download.oracle.com/otn-pub/java/jdk/8u191-b12/2787e4a523244c269598db4e85c51e0c/jdk-8u191-linux-x64.tar.gz
+tmp_dir=/tmp/$$
+
+test -e /etc/profile.d/java.sh || exit 1 
+
+cd ${tmp_dir} && wget --continue --no-check-certificate -O jdk-8u191-linux-x64.tar.gz --header "Cookie: oraclelicense=a" http://download.oracle.com/otn-pub/java/jdk/8u191-b12/2787e4a523244c269598db4e85c51e0c/jdk-8u191-linux-x64.tar.gz
+
+sudo test -d /data/app || mkdir  /data/app
+
+tar xf jdk-8u191-linux-x64.tar.gz -C /data/app/
+
+sudo echo "export JAVA_HOME=/data/app/jdk1.8.0_191
+export CLASSPATH=$JAVA_HOME/lib
+export PATH=$JAVA_HOME/bin:$PATH" >> /etc/profile.d/java.sh
