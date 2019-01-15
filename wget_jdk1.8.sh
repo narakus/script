23c9b5ab2a6251921870bcf13afb332ea5e2acaf
eli <liu11011241@yahoo.com>
2019-01-15 11:06:28 +0800

    Update wget_jdk1.8.sh

--- wget_jdk1.8.sh
+++ wget_jdk1.8.sh
@@ -2,7 +2,7 @@
 
 tmp_dir=/tmp/$$
 
-test -e /etc/profile.d/java.sh || exit 1 
+test -e /etc/profile.d/java.sh && exit 1 
 
 cd ${tmp_dir} && wget --continue --no-check-certificate -O jdk-8u191-linux-x64.tar.gz --header "Cookie: oraclelicense=a" http://download.oracle.com/otn-pub/java/jdk/8u191-b12/2787e4a523244c269598db4e85c51e0c/jdk-8u191-linux-x64.tar.gz
 
